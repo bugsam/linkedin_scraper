f59f365cc646af65441fa1b21c156349f61b59e6
Samuel Almeida <44240720+bugsam@users.noreply.github.com>
2023-03-24 23:19:12 -0300

    Update sample_people.py

--- samples/sample_people.py
+++ samples/sample_people.py
@@ -2,55 +2,122 @@ from linkedin_scraper import Person, actions
 from selenium import webdriver
 import xlsxwriter
 
-def createWorksheet(person, worksheet, row, col):
-    
-    worksheet.write_url(row, col, person.linkedin_url)
-    worksheet.write_string(row, col + 1, person.name)
-    worksheet.write_row(row, col + 1, person.also_viewed_urls)
-    worksheet.write_row(row, col + 1, person.about)
-    worksheet.write_row(row, col + 1, person.accomplishments)
-    worksheet.write_string(row, col + 1, person.company)
-    worksheet.write_string(row, col + 1, person.job_title)
-    worksheet.write_string(row, col + 1, person.location)
-    worksheet.write_boolean(row, col + 1, person.open_to_work)
-    
+def createWorksheet(person, workbook, row, row_ed, row_ex):
+
+    col = 0;
+    principal.write_url(row, col, person.linkedin_url)
+    principal.write_string(row, col, person.name)
+    col += 1
+    if person.also_viewed_urls:
+        for av in person.also_viewed_urls:
+            principal.write(row, col, av)
+            col += 1
+    else:
+        principal.write(row, col, "also_viewed_urls:empty")
+        col += 1
+
+    principal.write(row, col, person.about)
+    col += 1
+
+    if person.accomplishments:
+        for acc in person.accomplishments:
+            principal.write(row, col, person.accomplishments)
+            col += 1
+    else:
+        principal.write(row, col, "accomplishments:empty")
+        col += 1
+
+    principal.write_string(row, col, person.company)
+    col += 1
+
+    principal.write_string(row, col, person.job_title)
+    col += 1
+
+    principal.write_string(row, col, person.location)
+    col += 1
+
+    principal.write_boolean(row, col, person.open_to_work)
+    col += 1
+
     for ed in person.educations:
-        worksheet.write(row, col + 1, ed.company_size)
-        worksheet.write_string(row, col + 1, ed.degree)
+        col_ed = 0
+        educations.write(row_ed, col_ed, person.linkedin_url)
+        col_ed += 1
+        educations.write(row_ed, col_ed, ed.company_size)
+        col_ed += 1
+        educations.write_string(row_ed, col_ed, ed.degree)
+        col_ed += 1
+        
         if type(ed.description) is str: 
-            worksheet.write_string(row, col + 1, ed.description)
+            educations.write_string(row_ed, col_ed, ed.description)
         else:
-            worksheet.write_row(row, col + 1, "empty") 
-        worksheet.write(row, col + 1, ed.founded)
-        worksheet.write(row, col + 1, ed.from_date)
-        worksheet.write(row, col + 1, ed.headquarters)
-        worksheet.write(row, col + 1, ed.industry)
-        worksheet.write_string(row, col + 1, ed.institution_name)
-        worksheet.write_url(row, col + 1, ed.linkedin_url)
-        worksheet.write(row, col + 1, ed.to_date)
-        worksheet.write(row, col + 1, ed.type)
-        worksheet.write(row, col + 1, ed.website)
+            educations.write_row(row_ed, col_ed, "education_description:empty")
+        col_ed += 1
         
+        educations.write(row_ed, col_ed, ed.founded)
+        col_ed += 1    
+        
+        educations.write(row_ed, col_ed, ed.from_date)
+        col_ed += 1
+        
+        educations.write(row_ed, col_ed, ed.headquarters)
+        col_ed += 1
+        
+        educations.write(row_ed, col_ed, ed.industry)
+        col_ed += 1
+        
+        educations.write_string(row_ed, col_ed, ed.institution_name)
+        col_ed += 1
+        
+        educations.write_url(row_ed, col_ed, ed.linkedin_url)
+        col_ed += 1
+        
+        educations.write(row_ed, col_ed, ed.to_date)
+        col_ed += 1
+        
+        educations.write(row_ed, col_ed, ed.type)
+        col_ed += 1
+        
+        educations.write(row_ed, col_ed, ed.website)
+        row_ed += 1
+
     for ex in person.experiences:
-        worksheet.write(row, col + 1, ex.company_size)
+        col_ex = 0 
+        experiences.write(row_ex, col_ex, person.linkedin_url)
+        col_ex += 1
+        experiences.write(row_ex, col_ex, ex.company_size)
+        col_ex += 1
         if type(ex.description) is str:
-            worksheet.write_row(row, col + 1, ex.description)
+            experiences.write(row_ex, col_ex, ex.description)
         else:
-             worksheet.write_row(row, col + 1, "empty")
-        worksheet.write(row, col + 1, ex.duration)
-        worksheet.write(row, col + 1, ex.founded)
-        worksheet.write_row(row, col + 1, ex.from_date)
-        worksheet.write(row, col + 1, ex.headquarters)
-        worksheet.write(row, col + 1, ex.industry)
-        worksheet.write_row(row, col + 1, ex.institution_name)
-        worksheet.write_row(row, col + 1, ex.linkedin_url)
-        worksheet.write_row(row, col + 1, ex.location)
-        worksheet.write_row(row, col + 1, ex.position_title)
-        worksheet.write_row(row, col + 1, ex.to_date)
-        worksheet.write(row, col + 1, ex.type)
-        worksheet.write(row, col + 1, ex.website)
-        
-    return row
+             experiences.write(row_ex, col_ex, "empty")
+        col_ex += 1
+        experiences.write(row_ex, col_ex, ex.duration)
+        col_ex += 1
+        experiences.write(row_ex, col_ex, ex.founded)
+        col_ex += 1
+        experiences.write(row_ex, col_ex, ex.from_date)
+        col_ex += 1
+        experiences.write(row_ex, col_ex, ex.headquarters)
+        col_ex += 1
+        experiences.write(row_ex, col_ex, ex.industry)
+        col_ex += 1
+        experiences.write(row_ex, col_ex, ex.institution_name)
+        col_ex += 1
+        experiences.write(row_ex, col_ex, ex.linkedin_url)
+        col_ex += 1
+        experiences.write(row_ex, col_ex, ex.location)
+        col_ex += 1
+        experiences.write(row_ex, col_ex, ex.position_title)
+        col_ex += 1
+        experiences.write(row_ex, col_ex, ex.to_date)
+        col_ex += 1
+        experiences.write(row_ex, col_ex, ex.type)
+        col_ex += 1
+        experiences.write(row_ex, col_ex, ex.website)
+        row_ex += 1
+    
+    return row, row_ed, row_ex
 
 # driver
 options = webdriver.ChromeOptions()
@@ -65,17 +132,30 @@ password = "";
 people = ["jonmacaskill", "andrew-huberman"]
 
 # creating the Excel
-workbook = xlsxwriter.Workbook('people.xlsx')
-worksheet = workbook.add_worksheet()
+workbook = xlsxwriter.Workbook('2023-01.xlsx')
+principal = workbook.add_worksheet("principal")
+educations = workbook.add_worksheet("educations")
+experiences = workbook.add_worksheet("experiences")
 row = 0;
-col = 0;
+row_ed = 0
+row_ex = 0
 
 for p in people:
     driver = webdriver.Chrome(options=options)
-    actions.login(driver, email, password)
+    value = random.randint(1, 20)
+    driver.implicitly_wait(value)
+    try:
+        element = WebDriverWait(driver, 90).until(
+            actions.login(driver, email, password)
+        )
+    finally:
+        driver.quit()
+
     url = "https://www.linkedin.com/in/" + p
     person = Person(linkedin_url=url, driver=driver)
-    createWorksheet(person, worksheet, row, col)
+    value = random.randint(1, 10)
+    driver.implicitly_wait(value)
+    (row, row_ed, row_ex) = createWorksheet(person, workbook, row, row_ed, row_ex)
     row += 1
 
 workbook.close()
